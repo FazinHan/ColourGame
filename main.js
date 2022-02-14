7de7286a3ea6b84541a0fa2fc18da21e6729fdc3
Fizaan Khan <freak.fizaan@gmail.com>
2022-02-14 16:53:51 +0530

    Medium game mode

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const resetButton = document.getElementById("btn-reset");
 const tryagain = document.getElementById("try-again");
 const rgbElement = document.getElementById("rgb");
 const easyButton = document.getElementById("btn-easy");
+const mediumButton = document.getElementById("btn-medium");
 const hardButton = document.getElementById("btn-hard");
 const Canvas = document.getElementById("canvas");
 
@@ -27,13 +28,51 @@ const new_rgb = () => {
     return nrgb;
 }
 
+let umode = 'easy';
+
+function game_mode_easy()
+{
+    umode = 'easy';
+    easyButton.style.backgroundColor = 'white'; // highlighting game mode
+    easyButton.style.color = 'black';
+    hardButton.style.backgroundColor = 'transparent';
+    hardButton.style.color = 'antiquewhite';
+    mediumButton.style.backgroundColor = 'transparent';
+    mediumButton.style.color = 'antiquewhite';
+    resetButton.click()
+}
+
+function game_mode_hard()
+{
+    umode = 'hard';
+    hardButton.style.backgroundColor = 'white'; // highlighting game mode
+    hardButton.style.color = 'black';
+    easyButton.style.backgroundColor = 'transparent';
+    easyButton.style.color = 'antiquewhite';
+    mediumButton.style.backgroundColor = 'transparent';
+    mediumButton.style.color = 'antiquewhite';
+    resetButton.click()
+}
+
+function game_mode_medium()
+{
+    umode = 'medium';
+    hardButton.style.backgroundColor = 'transparent'; // highlighting game mode
+    hardButton.style.color = 'antiquewhite';
+    easyButton.style.backgroundColor = 'transparent';
+    easyButton.style.color = 'antiquewhite';
+    mediumButton.style.backgroundColor = 'white';
+    mediumButton.style.color = 'black';
+    resetButton.click()
+}
+
 function normalise(colour) // when called, makes everything the colour
 {
     let count;
     
     header.style.backgroundColor = `rgb(${colour[0]},${colour[1]},${colour[2]})`;
 
-    if(umode === 'easy')
+    if(umode === 'easy' || umode === 'medium')
     {
         count = 3;
     } else if (umode === 'hard') {
@@ -47,7 +86,7 @@ function normalise(colour) // when called, makes everything the colour
 
 function buildPallete(mode, correctRGB)
 {
-    if(mode === 'easy')
+    if(mode === 'easy' || umode === 'medium')
     {
         right = (Math.floor(Math.random()*100))%3;
         for(let i = 0; i < 3; i++)
@@ -57,6 +96,16 @@ function buildPallete(mode, correctRGB)
                 opts[i].style.backgroundColor = `rgb(${correctRGB[0]}, ${correctRGB[1]}, ${correctRGB[2]})`;
             } else {
                 let nrgb = new_rgb();
+                if(umode === 'medium'){
+                    let uncommon = Math.floor(Math.random()*10)%3;
+                    for(let i = 0; i < 3; i++)
+                    {
+                        if(i !== uncommon)
+                        {
+                            nrgb[i] = correctRGB[i];
+                        }
+                    }
+                }
                 opts[i].style.backgroundColor = `rgb(${nrgb[0]}, ${nrgb[1]}, ${nrgb[2]})`;
             }
         }
@@ -79,23 +128,22 @@ function buildPallete(mode, correctRGB)
     }
 }
 
-for(let i = 1; i <= 3; i++)
+for(let i = 0; i < 3; i++)
 {
-    const but = document.getElementById(`opt-${i}`);
-    but.style.left = `${topLeftx + (i-2) * (space) - 2}px`;
+    const but = opts[i];
+    but.style.left = `${topLeftx + (i - 1 + .5) * (space) - 2}px`;
     but.style.height = `${boxSize + 4}px`;
     but.style.width = `${boxSize + 4}px`;
 }
-for(let i = 1; i <= 3; i++)
+for(let i = 3; i < 6; i++)
 {
-    const but = document.getElementById(`opt-${i+3}`);
-    but.style.left = `${topLeftx + (i-2) * (space) - 2}px`;
+    const but = opts[i];
+    but.style.left = `${topLeftx + (i - 4 + .5) * (space) - 2}px`;
     but.style.height = `${boxSize + 4}px`;
     but.style.width = `${boxSize + 4}px`;
 }
 
 let RGBC = new_rgb(); // init target
-let umode = 'easy';
 easyButton.style.backgroundColor = 'white';
 easyButton.style.color = 'black';
 
@@ -126,27 +174,21 @@ resetButton.addEventListener("click", () => {
 // choose game mode
 
 easyButton.addEventListener("click", () => {
-    umode = 'easy';
-    easyButton.style.backgroundColor = 'white'; // highlighting game mode
-    easyButton.style.color = 'black';
-    hardButton.style.backgroundColor = 'transparent';
-    hardButton.style.color = 'antiquewhite';
-    resetButton.click()
+    game_mode_easy();
+})
+
+mediumButton.addEventListener("click", () => {
+    game_mode_medium();
 })
 
 hardButton.addEventListener("click", () => {
-    umode = 'hard';
-    hardButton.style.backgroundColor = 'white'; // highlighting game mode
-    hardButton.style.color = 'black';
-    easyButton.style.backgroundColor = 'transparent';
-    easyButton.style.color = 'antiquewhite';
-    resetButton.click()
+    game_mode_hard();
 })
 
 for(let i = 0; i < 6; i++) // final checking logic
 {
     opts[i].addEventListener("click", () => {
-        if( (umode === 'easy' && i < 3) || umode === 'hard'){
+        if( ( (umode === 'easy' || umode === 'medium') && i < 3) || umode === 'hard'){
             if(i === right){
                 tryagain.innerHTML = 'Correct!';
                 normalise(RGBC);
